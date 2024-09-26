4c12605b4510f8e99c86556a5c73ed2038eec436
rakeshkushwaha332 <53135128+rakeshkushwaha332@users.noreply.github.com>
2024-09-26 09:39:48 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx,ejs}', // Add all the extensions where you might use Tailwind classes
+    './public/index.html', // If applicable
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
